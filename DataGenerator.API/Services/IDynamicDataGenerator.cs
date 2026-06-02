021e4a3be99c3d454343a4f61d98daede858bb9c
mariela3009 <maranyramos7@gmail.com>
2026-06-01 22:17:23 -0500

    eliminar carpetas

--- DataGenerator.API/Services/IDynamicDataGenerator.cs
+++
@@ -1,8 +0,0 @@
-using DataGenerator.API.Models;
-
-namespace DataGenerator.API.Services;
-
-public interface IDynamicDataGenerator
-{
-    List<Dictionary<string, object>> GenerateData(DynamicTableRequest request);
-}
