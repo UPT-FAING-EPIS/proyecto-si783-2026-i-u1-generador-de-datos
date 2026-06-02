d582475dee62f32d1ad79cb7913d71a45be8da0f
mariela3009 <maranyramos7@gmail.com>
2026-06-01 22:12:44 -0500

    Eliminar carpetas innecesarias1

--- DataGenerator.API/Exporters/IDynamicExporter.cs
+++
@@ -1,9 +0,0 @@
-using DataGenerator.API.Models;
-
-namespace DataGenerator.API.Exporters;
-
-public interface IDynamicExporter
-{
-    string Export(List<Dictionary<string, object>> data, DynamicTableRequest request);
-    string GetFileExtension();
-}
